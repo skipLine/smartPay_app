f3a4051170e40a342cbb19a6e53ebf2c09481010
Hong Cheng <hong_cheng@berkeley.edu>
2012-03-28 21:56:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartPayApp::Application.config.secret_token = '42b690c0c06682933b968207eb6a0dbf3757958ff760d7edaee4f471a8b164146658cc21b0cd1a6883c36422360e993215c0b6ee178dee492cebeb0c4c0d7e70'
