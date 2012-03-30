b44773da27992ab0c64f517397bf8b21dd200f0f
Hong Cheng <hong_cheng@berkeley.edu>
2012-03-30 15:32:18 -0700

    modify

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Smart Payment Application"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Smart Payment Application | Home")
+                        :content => @base_title + "| Home")
     end
   end
 
@@ -25,8 +29,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Smart Payment Application | Contact")
+                        :content => @base_title + "| Contact")
     end
   end
 
@@ -39,8 +42,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Smart Payment Application | About")
+                        :content => @base_title + "| About")
     end
   end
 end
